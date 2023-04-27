c671e9bebc09a256dcd9b9aa01ed715b3515bcdb
Axel <cymbalaxel@gmail.com>
2023-04-27 15:41:41 -0300

    se agrego una estructura simple a los files js de cada uno

--- src/componentes/Productos/index.js
+++ src/componentes/Productos/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-export const Header = () => {
+export const ProductosLista = () => {
     return (
         <div>
-            <h1>Header</h1>
+            <h1>Productos</h1>
         </div>
     )
 }
---
+++ src/componentes/paginas.js
@@ -0,0 +1,9 @@
+import React from "reacr";
+
+export const paginas = () => {
+    return (
+        <div>
+            <h1>Paginas</h1>
+        </div>
+    )
+}
