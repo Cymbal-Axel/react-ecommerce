5b495dd296dde50a1f219c52a0b615d704cb3f3d
Axel <cymbalaxel@gmail.com>
2023-04-28 14:26:29 -0300

    se borro el icono de menu y se agrego 'inicio', 'productos' y el carrito

--- src/componentes/Header/index.js
+++ src/componentes/Header/index.js
@@ -1,9 +1,28 @@
 import React from "react";
+import Nike from "../../images/Nike.jpg";
 
 export const Header = () => {
     return (
         <div>
-            <h1>Header</h1>
+            <header>
+                <a href="#">
+                    <div className="logo">
+                        <img src={Nike} alt=""/>
+                    </div>
+                </a>
+                <ul>
+                    <li>
+                        <a href="#">INICIO</a>
+                    </li>
+
+                    <li>
+                        <a href="#">PRODUCTOS</a>
+                    </li>
+                </ul>
+                <div className="cart">
+                    <box-icon name="cart"></box-icon>
+                </div>
+            </header>
         </div>
     )
 }
