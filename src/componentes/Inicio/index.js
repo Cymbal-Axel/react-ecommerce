d57290fa0d99d9e968200f63baac7d4185ff86c9
Axel <cymbalaxel@gmail.com>
2023-05-16 17:01:01 -0300

    se contruyo una parte del inicio

--- src/componentes/Inicio/index.js
+++ src/componentes/Inicio/index.js
@@ -4,8 +4,14 @@ import { Link } from 'react-router-dom';
 
 export const Inicio = () => {
     return(
-        <div>
-            <h1>Inicio</h1>
+        <div className="inicio">
+            <Link to="/">
+                <h1>Inicio</h1>
+            </Link>
+            <Link to="/productos">
+                <h1>Productos</h1>
+            </Link>
+            <img src={Portada} alt="inicio"/>
         </div>
     )
 }
