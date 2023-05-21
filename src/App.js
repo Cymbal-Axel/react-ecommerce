4243fe8e896176c882a5ce73248123280620e501
Axel <cymbalaxel@gmail.com>
2023-05-21 17:05:52 -0300

    se importo Dataprovider y se aplico en la funcion

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import {Header} from './componentes/Header';
+import { Header } from './componentes/Header';
 import 'boxicons';
-import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { Paginas } from './componentes/Paginas';
+import { DataProvider } from './context/Dataprovider'
 
 function App() {
   return (
+    <DataProvider>
     <div className="App">
       <Router>
         <Header />
         <Paginas />
       </Router>
     </div>
+    </DataProvider>
   );
 }
 
