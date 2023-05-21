9d0d7caadf0cbccdad53ddc81f30b7a3ec8e58f5
Axel <cymbalaxel@gmail.com>
2023-05-21 18:11:48 -0300

    se hizo un mapeo de los productos que renderiza el componente ProductoItem con sus atributos

--- src/componentes/Productos/index.js
+++ src/componentes/Productos/index.js
@@ -13,7 +13,19 @@ export const ProductosLista = () => {
         <>
             <h1 className="title">PRODUCTOS</h1>
             <div className="productos">
-                <ProductoItem />
+                {
+                productos.map(producto => (
+                    <ProductoItem 
+                    key={producto.id}
+                    id={producto.id}
+                    title={producto.title}
+                    price={producto.price}
+                    image={producto.image}
+                    category={producto.category}
+                    cantidad={producto.cantidad}
+                    />
+                ))
+                }
                 
             </div>
         
