90e801f7d821a48bd766531c90dc86a27d2eb8ba
Axel <cymbalaxel@gmail.com>
2023-05-26 20:04:08 -0300

    se agrego un .map de los productos para que renderice cada producto aniadido al carrito

--- src/componentes/Carrito/index.js
+++ src/componentes/Carrito/index.js
@@ -5,6 +5,7 @@ import { DataContext } from '../../context/Dataprovider'
 export const Carrito = () =>{
     const value = useContext(DataContext);
     const [menu, setMenu] = value.menu;
+    const [carrito, setCarrito] = value.carrito
 
     const togglefalse = () => {
         setMenu(false);
@@ -22,21 +23,23 @@ export const Carrito = () =>{
                 </div>
                 <h2>Su carrito</h2>
                 <div className='carrito__center'>
-                    <div className='carrito__item'>
-                        <img src={Card} alt=''/>
+                    {
+                        carrito.map((producto)=>
+                        <div className='carrito__item'>
+                        <img src={producto.image} alt=''/>
                         <div>
-                            <h3>title</h3>
-                            <p className='price'>$345</p>
+                            <h3>{producto.title}</h3>
+                            <p className='price'>${producto.price}</p>
                         </div>
                         <div>
                             <box-icon name='up-arrow' type='solid'></box-icon>
-                            <p className='cantidad'>1</p>
+                            <p className='cantidad'>{producto.cantidad}</p>
                             <box-icon name='down-arrow' type='solid'></box-icon>
                         </div>
                         <div className='remove__item'>
                             <box-icon name='trash'></box-icon>
                         </div>
-                    </div>
+                    </div>)}
                 </div>
                 <div className='carrito__footer'>
                     <h3>Total: $2334</h3>
