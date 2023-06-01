9bbb249686e4c889622d5b9bdda00bf30491be77
Axel <cymbalaxel@gmail.com>
2023-06-01 19:05:21 -0300

    se creo un estado TOTAL que contiene el valor del precio total de los productos, se agrego un useEffect con una funcion "getTotal" que suma los precios de los productos existentes en el carrito. Tambien se agrego el estado al objeto "value" para ser utilizado en el file index.js del componente "Carrito"

--- src/context/Dataprovider.js
+++ src/context/Dataprovider.js
@@ -7,6 +7,7 @@ export const DataProvider = (props) => {
     const [productos, setProductos] = useState([]);
     const [menu, setMenu] = useState(false);
     const [carrito, setCarrito] = useState([]);
+    const [total, setTotal] = useState(0)
 
     useEffect(() => {
         const producto = Data.items
@@ -47,14 +48,23 @@ export const DataProvider = (props) => {
     }, [carrito])
     
 
-
+    useEffect(() => {
+        const getTotal= () => {
+            const res = carrito.reduce((prev, item)=> {
+                return prev + (item.price * item.cantidad)
+            }, 0)
+            setTotal(res)
+        }
+        getTotal()
+    },[carrito])
 
 
     const value = {
         productos : [productos],
         menu: [menu, setMenu],
         addCarrito: addCarrito,
-        carrito: [carrito, setCarrito]
+        carrito: [carrito, setCarrito],
+        total: [total, setTotal]
     }
 
     return(
