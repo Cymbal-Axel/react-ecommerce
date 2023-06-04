23a0125fd8309099bf4313455d09ba15a243287c
Axel <cymbalaxel@gmail.com>
2023-06-04 17:47:49 -0300

    se agrego una ruta, y se importo ProductoDetalles

--- src/componentes/Paginas.js
+++ src/componentes/Paginas.js
@@ -2,6 +2,7 @@ import React from "react";
 import {BrowserRouter as Router, Switch, Route, Routes, Link} from "react-router-dom";
 import { Inicio } from './Inicio';
 import { ProductosLista } from './Productos/index.js';
+import { ProductoDetalles } from "./Productos/ProductoDetalles";
 
 export const Paginas = () => {
     return (
@@ -9,6 +10,7 @@ export const Paginas = () => {
             <Routes>
                 <Route path="/" element={<Inicio />}/>
                 <Route exact path="/productos"  element={<ProductosLista />} />
+                <Route path='/producto/:id' element={<ProductoDetalles />} />
             </Routes>
         </section>
     )
