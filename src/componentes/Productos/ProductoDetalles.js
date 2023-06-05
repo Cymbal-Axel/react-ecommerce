98fc552924ccb70a2ef7dcd7800db31bd1205ba4
Axel <cymbalaxel@gmail.com>
2023-06-05 17:00:07 -0300

    se agrego un estado detalle, se uso useParams, se creo un useEffect y se empezo a crear el maquetado

--- src/componentes/Productos/ProductoDetalles.js
+++ src/componentes/Productos/ProductoDetalles.js
@@ -3,9 +3,45 @@ import { DataContext } from '../../context/Dataprovider';
 import { useParams } from 'react-router-dom';
 
 export const ProductoDetalles = () =>{
+
+    const value = useContext(DataContext);
+    const [productos] = value.productos;
+    const params = useParams();
+    const [detalle, setDetalle] = useState([]);
+
+    useEffect(()=>{
+        productos.forEach(producto => {
+            if(producto.id === parseInt(params.id)){
+                setDetalle(producto);
+            }  
+        })
+    },[params.id, productos])
+console.log(detalle)
     return (
-        <div>
-            <h1> Detalles del producto</h1>
-        </div>
+        <>
+        {
+            <div className="detalles">
+                <h2>{detalle.title}</h2>
+                <p className="price">${detalle.price}</p>
+                <div className='grid'>
+                    <p className="nuevo">Nuevo</p>
+                    <div className="size">
+                        <select placeholder="tamaño">
+                            <option value="1">1</option>
+                            <option value="1">2</option>
+                            <option value="1">3</option>
+                            <option value="1">4</option>
+                            <option value="1">5</option>
+                            <option value="1">6</option>
+                            <option value="1">7</option>
+                            <option value="1">8</option>
+                            <option value="1">9</option>
+                        </select>
+                        <p>Tamaño</p>
+                    </div>
+                </div>
+            </div>
+        }
+        </>
     )
 }
